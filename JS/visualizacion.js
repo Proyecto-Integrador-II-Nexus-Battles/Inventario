8cb754d1c04f483f88ae4096607849ef812a502e
kintanuki <juandiquintanareyes@gmail.com>
2024-02-28 16:29:49 -0500

    query

--- JS/visualizacion.js
+++ JS/visualizacion.js
@@ -7,10 +7,14 @@ function abrir() {
 function cerrar() {
     document.getElementById('tipocarta').style.display = 'none';
     document.getElementById('tipoheroe').style.display = 'none';
+    document.getElementById('tipoitem').style.display = 'none';
+
 ;
 }
 
 function abrir_tipo() {
     document.getElementById('tipoheroe').style.display = 'block';
     document.getElementById('tipocarta').style.display = 'none';
+    document.getElementById('tipoitem').style.display = 'block';
+    document.getElementById('tipoheroe').style.display = 'none';
 }
