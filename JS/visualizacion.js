979b5119c8620997b901fd0bd3cdeeb42c8464b7
kintanuki <juandiquintanareyes@gmail.com>
2024-02-27 19:21:53 -0500

    actualizacion

--- JS/visualizacion.js
+++ JS/visualizacion.js
@@ -2,8 +2,15 @@ document.getElementById('carta').addEventListener('click', abrir);
 
 function abrir() {
     document.getElementById('tipocarta').style.display = 'block';
+
 }
 function cerrar() {
     document.getElementById('tipocarta').style.display = 'none';
+    document.getElementById('tipoheroe').style.display = 'none';
+;
 }
 
+function abrir_tipo() {
+    document.getElementById('tipoheroe').style.display = 'block';
+    document.getElementById('tipocarta').style.display = 'none';
+}
