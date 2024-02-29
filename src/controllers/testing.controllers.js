fc9745f624d3323c0f4ab2058f844e3f4e623f1d
kintanuki <juandiquintanareyes@gmail.com>
2024-02-29 14:43:08 -0500

    ejs

---
+++ src/controllers/testing.controllers.js
@@ -0,0 +1,68 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const defaultR = (req, res) => {
+  const datos = [
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+  ];
+  console.log(datos);
+  res.render("visualizacion_inventario", { datos });
+};
+
+// export const defaultR2 = (req, res) => {
+//   res.render("visualizacion_inventario");
+// };
+
